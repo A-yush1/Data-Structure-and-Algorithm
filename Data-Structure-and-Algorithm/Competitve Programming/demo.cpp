b085bf15c9634a6f61ea5807c6e8651b41d7e905
A-yush1 <ayush123kumar34@gmail.com>
2024-05-31 16:07:29 +0530

    created folder

--- Data-Structure-and-Algorithm/Competitve Programming/demo.cpp
+++ Data-Structure-and-Algorithm/Competitve Programming/demo.cpp
@@ -6,23 +6,15 @@ int main()
 {
 
 ios_base::sync_with_stdio(false); 
-    cin.tie(NULL);    
+  cin.tie(NULL);    
 
   #ifndef ONLINE_JUDGE
   freopen("input.txt","r",stdin);
   freopen("output.txt","w",stdout);
   #endif
 
- ll n;
- cin>>n;
- int arr[n];
- for(int i=0; i<n; i++)
- {
-  cin>>arr[i];
- }
 
- for(int i=0; i<n; i++)
- cout<<arr[i]<<"* ";
+
 
   
 return 0;  
