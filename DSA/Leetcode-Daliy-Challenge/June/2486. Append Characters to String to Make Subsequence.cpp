2f61a75368915d8f645f4cbbc4f5c2e83881c1bc
A-yush1 <ayush123kumar34@gmail.com>
2024-06-03 23:56:57 +0530

    3 June

---
+++ DSA/Leetcode-Daliy-Challenge/June/2486. Append Characters to String to Make Subsequence.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        
+        int n = s.length(), m = t.length();
+        int i = 0, j = 0;
+
+        while(i < n && j < m)
+        {
+            if(s[i] == t[j])
+            j++;
+
+            i++;
+
+        }
+
+        return (m - j);
+    }
+};
